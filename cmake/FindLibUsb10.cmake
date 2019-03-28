37114bb3f94280acb19c9d14fbcf153cee83fd5a
Krzysztof Kapuscik <k.kapuscik@gmail.com>
2019-03-28 10:59:25 +0100

    Additional compilation fixes, debug prefixes added

--- cmake/FindLibUsb10.cmake
+++ cmake/FindLibUsb10.cmake
@@ -1,71 +1,25 @@
-# - Try to find libusb-1.0
-# Once done this will define
-#
-# LIBUSB10_FOUND - system has libusb
-# LIBUSB10_INCLUDE_DIRS - the libusb include directory
-# LIBUSB10_LIBRARIES - Link these to use libusb
-# LIBUSB10_DEFINITIONS - Compiler switches required for using libusb
-#
-# Adapted from cmake-modules Google Code project
-#
-# Copyright (c) 2006 Andreas Schneider <mail@cynapses.org>
-#
-# (Changes for libusb) Copyright (c) 2008 Kyle Machulis <kyle@nonpolynomial.com>
-#
-# (Changes) Copyright (c) 2019 Krzysztof Kapuscik
-#
-# Redistribution and use is allowed according to the terms of the New BSD license.
-#
-# CMake-Modules Project New BSD License
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# * Redistributions of source code must retain the above copyright notice, this
-# list of conditions and the following disclaimer.
-#
-# * Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-#
-# * Neither the name of the CMake-Modules Project nor the names of its
-# contributors may be used to endorse or promote products derived from this
-# software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-
 include(MiscHelpers)
 
 find_path(LIBUSB10_INCLUDE_DIR
 	NAMES
 		libusb-1.0/libusb.h
 		libusb.h
-	PATHS
-		/usr/include
-		/usr/local/include
-		/sw/include
 	PATH_SUFFIXES
 		libusb-1.0
 )
 
 find_library_release(LIBUSB10_LIBRARY_RELEASE
-    NAMES usb-1.0 usb libusb-1.0
-    PATHS /usr/lib /usr/local/lib
+    NAMES
+        usb-1.0
+        usb
+        libusb-1.0
 )
 
 find_library_debug(LIBUSB10_LIBRARY_DEBUG
-    NAMES usb-1.0 usb libusb-1.0
-    PATHS /usr/lib /usr/local/lib
+    NAMES
+        usb-1.0
+        usb
+        libusb-1.0
 )
 
 include(FindPackageHandleStandardArgs)
